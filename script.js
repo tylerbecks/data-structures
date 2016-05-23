c835f911a2228e8f6bf951e818b2f57494bd9c84
Tyler Becks <tylerdbecks@gmail.com>
2016-05-23 11:31:06 -0700

    Add doublylinkedilsts

--- script.js
+++ script.js
@@ -99,3 +99,84 @@ LinkedList.prototype = {
 	}
 
 }
+
+
+// Create the DoublyLinkedList Constructor
+function DoublyLinkedList() {
+	this._length = 0;
+	this._head = null;
+	this._tail = null;
+};
+
+DoublyLinkedList.prototype = {
+
+	add: function(data) {
+
+		var node = {
+			data: data,
+			next: null,
+			prev: null
+		};
+
+		if(this._length === 0) {
+			this._head = node;
+			this._tail = node;
+		} else {
+			this._tail.next = node;
+			node.prev = this._tail;
+			this._tail = node;
+		}
+
+		//don't forget to update the count
+		this._length++;
+	},
+
+	remove: function(index) {
+
+		//check for out-of-bounds values
+		if(index > -1 && index < this._length) {
+
+			var current = this._head;
+			var i = 0;
+
+			//special case: removing the first item
+			if(index === 0) {
+				this._head = current.next;
+
+                /*
+                 * If there's only one item in the list and you remove it,
+                 * then this._head will be null. In that case, you should
+                 * also set this._tail to be null to effectively destroy
+                 * the list. Otherwise, set the previous pointer on the
+                 * new this._head to be null.
+                 */
+
+                 if(!this._head) {
+                 	this._tail = null;
+                 } else {
+                 	this._head.prev = null;
+                 }
+
+			//special case: removing the last item
+			}else if(index === this._length - 1) {
+				current = this._tail;
+				this._tail = current.prev;
+				this._tail.next = null;
+			}else {
+
+				//find the right location
+				while(i++ < index) {
+					current = current.next;
+				}
+
+				//skip over the item to remove
+				current.prev.next =  current.next;
+				current.next.prev = current.prev;
+			}
+		} else {
+			return null;
+		}
+	},
+
+	
+}
